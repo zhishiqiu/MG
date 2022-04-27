722b1be01d325c39e5298fa15aff51e6ea1c7c47
halikes <54992344+halikes@users.noreply.github.com>
2022-04-27 14:16:50 +0900

    login
    add code

--- front/login.php
+++ front/login.php
@@ -6,6 +6,14 @@
 
     if ($username and $password){
         $sql = "select * from login where username = '$username' and password = '$password' ";
+        $result = $conn->query($sql4);
+        if ($result->num_rows > 0){
+            header("refresh:0;url=index.html");
+        }else{
+            echo "<script>alert('잘못된 계정이나 비밀번호!');</script>";
+
+            header("refresh:0;url=login-.php");
+        }
     }else{
         echo "<script>alert('please enter you username or password');<script>";
 
