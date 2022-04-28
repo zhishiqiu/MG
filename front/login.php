067092a883417543c18bf2c869e374e2512d8da0
halikes <54992344+halikes@users.noreply.github.com>
2022-04-28 17:19:08 +0900

    login
    session

--- front/login.php
+++ front/login.php
@@ -1,19 +1,25 @@
 <?php
     include('connect.php');
-
-    $username = $_POST['username'];
-    $password = $_password['password'];
-
+    // session_start();
+    if(isset($_POST['username'])){
+        $username = $_POST['username'];
+    }
+    if(isset($_POST['password'])){
+        $password = $_POST['password'];
+    }
+   
     if ($username and $password){
+       
         $sql = "select * from login where username = '$username' and password = '$password' ";
-        $result = $conn->query($sql4);
+        $result = $conn->query($sql);
         if ($result->num_rows > 0){
-            header("refresh:0;url=index.html");
+             header("location=index.html");
         }else{
-            echo "<script>alert('잘못된 계정이나 비밀번호!');</script>";
+             echo "<script>alert('잘못된 계정이나 비밀번호!');</scrip>";
 
-            header("refresh:0;url=login-.php");
+             header("refresh:0;url=login.html");
         }
+        
     }else{
         echo "<script>alert('please enter you username or password');<script>";
 
