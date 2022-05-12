b63362375d88d4972726e6b82e62d922beda16f4
halikes <54992344+halikes@users.noreply.github.com>
2022-05-12 12:04:07 +0900

    addpage
    创建一下文件

--- pet_adoption_website/login.php
+++ pet_adoption_website/login.php
@@ -1,6 +1,6 @@
 <?php
     include "connect.php";
-    session_start();
+    // session_start();
     $username = $_POST['username'];
     $password = $_POST['password'];
    
@@ -15,6 +15,7 @@ if($username == "root" and $password == "12345"){
         if ($result->num_rows > 0){
              setcookie('username',$username,time() + 24 * 3600,'/');
              setcookie('password',$password,time() + 24 * 3600,'/');
+             echo "<script>alert('welcome! ?username=$username');</script>";
              header("refresh:0;url=index.php");
         }else{
              echo "<script>alert('worry username or password');</scrip>";
--- pet_adoption_website/loginpage.php
+++ pet_adoption_website/loginpage.php
@@ -1,3 +1,9 @@
+<?php
+    if(!isset($_COOKIE['username'])){
+        header("refresh:0;url=userpage.php");
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
